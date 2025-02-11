d4334bd0cb374bb277cccd70d871f8aac6ce18f1
Lev Jad <levjad@gmail.com>
2025-02-11 21:21:50 +0100

    Extended synonyms & antonyms

--- src/components/WordResult.tsx
+++ src/components/WordResult.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import AudioPlayer from "./AudioPlayer";
 import ErrorMessage from "./ErrorMessage.tsx";
 import LoadingIndicator from "./LoadingIndicator.tsx";
+import {FaExternalLinkAlt} from "react-icons/fa";
 
 interface WordResultProps {
     word: string;
@@ -16,6 +17,8 @@ interface Definition {
 interface Meaning {
     partOfSpeech: string;
     definitions: Definition[];
+    synonyms: string[];
+    antonyms: string[];
 }
 
 interface WordData {
@@ -56,7 +59,7 @@ const WordResult: React.FC<WordResultProps> = ({ word }) => {
                 <div key={idx} className="py-2">
                     <div className="divider divider-start italic font-semibold my-8">{meaning.partOfSpeech}</div>
                     <h2 className="text-lg text-gray-700 font-semibold my-3">Meanings</h2>
-                    <ul className="list-disc pl-10 marker:text-blue-500 space-y-3">
+                    <ul className="list-disc pl-10 marker:text-blue-500 space-y-4">
                         {meaning.definitions.map((def, index) => (
                             <li key={index}>
                                 {def.definition}
@@ -64,11 +67,26 @@ const WordResult: React.FC<WordResultProps> = ({ word }) => {
                             </li>
                         ))}
                     </ul>
+                    {meaning.synonyms.length > 0 && (
+                        <div className="flex gap-3 my-3 items-center">
+                            <h2 className="text-lg text-gray-700 font-semibold my-3">Synonyms</h2>
+                            <span className="text-blue-500 text-lg">{meaning.synonyms.join(', ')}</span>
+                        </div>
+                    )}
+                    {meaning.antonyms.length > 0 && (
+                        <div className="flex gap-2 my-3 items-center">
+                            <h2 className="text-lg text-gray-700 font-semibold my-3">Antonyms</h2>
+                            <span className="text-blue-500 text-lg">{meaning.antonyms.join(', ')}</span>
+                        </div>
+                    )}
                 </div>
             ))}
+            <div className="divider"></div>
             {data.sourceUrls && (
-                <p className="mt-6 text-sm">
-                    Source <a href={data.sourceUrls[0]} className="text-blue-500 underline" target="_blank" rel="noopener noreferrer">{data.sourceUrls[0]}</a>
+                <p className="flex gap-2 items-center justify-center mt-2 text-sm italic text-gray-700">
+                    <span className="font-semibold mr-3">Source</span>
+                    <a href={data.sourceUrls[0]} className="hover:text-blue-500 underline" target="_blank" rel="noopener noreferrer">{data.sourceUrls[0]}</a>
+                    <FaExternalLinkAlt />
                 </p>
             )}
         </div>
